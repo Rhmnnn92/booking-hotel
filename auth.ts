5d30661fe9a099d0200a30f62dbbc787a86fc854
rohman <rohmannnnn14@gmail.com>
2026-03-16 18:44:25 +0700

    instalasi prisma, koneksi postgres vercel, membuat login, sign out

--- auth.ts
+++ auth.ts
@@ -1,9 +1,25 @@
 import NextAuth from "next-auth";
+import { prisma } from "./lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  // @ts-expect-error - Custom prisma generator path causes type mismatch with @auth/prisma-adapter
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
-  session:{ strategy:"jwt"},
-  pages:{
-    signIn:"/signin",
+  session: { strategy: "jwt" },
+  pages: {
+    signIn: "/signin",
   },
+
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    session({ session, token }) {
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      return session;
+    },
+  }
 })
--- components/navbar/navlink.tsx
+++ components/navbar/navlink.tsx
@@ -1,58 +1,140 @@
-'use client';
+"use client";
 import Link from "next/link";
 import { useState } from "react";
-import {IoClose, IoMenu} from "react-icons/io5";
+import Image from "next/image";
+import { IoClose, IoMenu } from "react-icons/io5";
+import { useSession, signOut } from "next-auth/react";
 import clsx from "clsx";
 
 const Navlink = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const { data: session } = useSession();
   return (
     <>
-    <button onClick={() => setOpen(!open)} className='inline-flex items-center p-2 justify-center text-sm text-gray-500 rounded-md md:hidden hover:bg-gray-100 '>
-{!open ? <IoMenu className="size-8" /> : <IoClose className="size-8" />}
-        </button>
-        {/* supaya beda di desktop dan mobile */}
-        <div className={clsx("w-full md:block md:w-auto", {
-            "hidden": !open
-        } )}>
-<ul className="flex flex-col font-semibold text-sm uppercase p-4 mt-4 rounded-sm bg-gray-50 md:flex-row md:items-center md:space-x-10 md:p-0 md:mt-0 md:border-0 md:bg-white">
-<li>
-<Link href="/" className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0">Home</Link>
-</li>
-
-<li>
-<Link href="/about" className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0">About</Link>
-</li>
-
-<li>
-<Link href="/room" className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0">room</Link>
-</li>
-
-<li>
-<Link href="/contact" className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0">contact</Link>
-</li>
-
-<li>
-<Link href="/myreservation" className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0">My Reservation</Link>
-</li>
-
-<li>
-<Link href="/admin/dashboard" className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0">Dashboard</Link>
-</li>
-
-<li>
-<Link href="/admin/room" className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0">Manage Rooms</Link>
-</li>
-
-<li className="pt-2 md:pt-0">
-    <Link href="/signin" className="py-2.5 px-6 bg-orange-400 text-white hover:bg-orange-500 rounded-sm">Sign In</Link>
-
-</li>
-
-</ul>
+      {session?.user ? (
+        <div className="flex items-center gap-3 justify-end md: order-2">
+          <div className="hiddedn text-sm bg-gray-50 border rounded-full md:me-0 md: block focus:ring-4 focus:ring-gray-300">
+            <Image
+              className="size-9 object-cover rounded-full over"
+              src={session.user.image || "/file.svg"}
+              width={40}
+              height={40}
+              alt="/avatar"
+            />
+          </div>
+          <button
+            onClick={() => signOut()}
+            className="md:block hidden py-2 px-4 bg-gray-50 text-gray-700 hover:bg-gray-100 rounded-sm cursor-pointer"
+          >
+            Sign Out
+          </button>
         </div>
-        </>
-  )
-}
+      ) : null}
 
-export default Navlink
+      <button
+        onClick={() => setOpen(!open)}
+        className="inline-flex items-center p-2 justify-center text-sm text-gray-500 rounded-md md:hidden hover:bg-gray-100 "
+      >
+        {!open ? <IoMenu className="size-8" /> : <IoClose className="size-8" />}
+      </button>
+      {/* supaya beda di desktop dan mobile */}
+      <div
+        className={clsx("w-full md:block md:w-auto", {
+          hidden: !open,
+        })}
+      >
+        <ul className="flex flex-col font-semibold text-sm uppercase p-4 mt-4 rounded-sm bg-gray-50 md:flex-row md:items-center md:space-x-10 md:p-0 md:mt-0 md:border-0 md:bg-white">
+          <li>
+            <Link
+              href="/"
+              className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+            >
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/about"
+              className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+            >
+              About
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/room"
+              className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+            >
+              room
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/contact"
+              className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+            >
+              contact
+            </Link>
+          </li>
+          {session && (
+            <>
+              <li>
+                <Link
+                  href="/myreservation"
+                  className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+                >
+                  My Reservation
+                </Link>
+              </li>
+              {session.user.role === "admin" && (
+                <>
+                  <li>
+                    <Link
+                      href="/admin/dashboard"
+                      className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      href="/admin/room"
+                      className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+                    >
+                      Manage Rooms
+                    </Link>
+                  </li>
+                </>
+              )}
+            </>
+          )}
+          {session ? (
+            <li className="pt-2 md:pt-0">
+              <button
+                onClick={() => signOut()}
+                className="md:hidden  py-2.5 px-4 bg-red-400 text-white hover:bg-red-600 rounded-sm cursor-pointer"
+              >
+                Sign Out
+              </button>
+            </li>
+          ) : (
+            <li className="pt-2 md:pt-0">
+              <Link
+                href="/signin"
+                className="py-2.5 px-6 bg-orange-400 text-white hover:bg-orange-500 rounded-sm"
+              >
+                Sign In
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navlink;
